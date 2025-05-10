eb30cef6f9fdd342022ce64619421a4b1417136e
yuth <yuth@todo.com>
2025-05-10 17:02:50 +0900

    update

--- aljnu_path/include/aljnu_path/bezier.h
+++ aljnu_path/include/aljnu_path/bezier.h
@@ -1,31 +1,40 @@
-#include <geometry_msgs/msg/point.hpp>
+#ifndef ALJNU_BEZIER_PATH_H
+#define ALJNU_BEZIER_PATH_H
+
 #include <geometry_msgs/msg/pose_stamped.hpp>
-#include <geometry_msgs/msg/vector3.hpp>
 #include <interactive_markers/interactive_marker_server.hpp>
 #include <nav_msgs/msg/path.hpp>
 #include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/color_rgba.hpp>
 #include <trajectory_msgs/msg/joint_trajectory.hpp>
 #include <visualization_msgs/msg/interactive_marker.hpp>
 #include <visualization_msgs/msg/interactive_marker_control.hpp>
 #include <visualization_msgs/msg/marker.hpp>
 
-using TrajectoryMsg = trajectory_msgs::msg::JointTrajectory;
-using TrajectoryPointMsg = trajectory_msgs::msg::JointTrajectoryPoint;
 using PoseStampedMsg = geometry_msgs::msg::PoseStamped;
 using PathMsg = nav_msgs::msg::Path;
+using IntMarkerFBMsg = visualization_msgs::msg::InteractiveMarkerFeedback;
 
 class Bezier : public rclcpp::Node {
     private:
-        rclcpp::Publisher<TrajectoryMsg>::SharedPtr pub_;
-        rclcpp::Subscription<PoseStampedMsg>::SharedPtr sub_;
+        std::unique_ptr<interactive_markers::InteractiveMarkerServer> server_;
+        rclcpp::Publisher<PathMsg>::SharedPtr path_publisher_;
 
+        std::array<double, 2> p0;
+        std::array<double, 2> p1;
+        std::array<double, 2> p2;
+        std::array<double, 2> p3;
 
-        void goal_callback(const PoseStampedMsg::ConstPtr &msg);
+        void process_feedback(const IntMarkerFBMsg::ConstSharedPtr &feedback);
+        void create_marker(const std::string &name, double x, double y, double z);
+        void publish_path();
 
     public:
         Bezier();
-        void compute_pose(double t);
+        ~Bezier();
+
+        void compute_pose(double t, double &x, double &y);
         void compute_velo(double t);
         void compute_accl(double t);
 };
+
+#endif
--- aljnu_path/src/bezier.cpp
+++ aljnu_path/src/bezier.cpp
@@ -1,38 +1,126 @@
 #include "aljnu_path/bezier.h"
 
 Bezier::Bezier() : Node("bezier_path") {
-    pub_ = this->create_publisher<TrajectoryMsg>("path", 10);
-    sub_ = this->create_subscription<PoseStampedMsg>(
-        "goal", 10, std::bind(&Bezier::goal_callback, std::placeholders::_1));
+    server_ = std::make_unique<interactive_markers::InteractiveMarkerServer>(
+        "marker", this);
+    path_publisher_ = this->create_publisher<PathMsg>("path", 10);
+
+    p0 = {0.0, 0.0};
+    p1 = {0.0, 0.0};
+    p2 = {0.0, 0.0};
+    p3 = {0.0, 0.0};
+
+    create_marker("start", 0.0, 0.0, 0.0);
+    create_marker("c1", 0.0, 0.0, 0.0);
+    create_marker("c2", 0.0, 0.0, 0.0);
+    create_marker("end", 0.0, 0.0, 0.0);
+
+    server_->applyChanges();
 }
 
-void Bezier::goal_callback(const PoseStampedMsg::ConstPtr &msg) {
-    msg->pose.position.x;
-    msg->pose.position.x;
-    msg->pose.position.x;
+Bezier::~Bezier() {
 }
-void Bezier::compute_pose(double t) {
+
+void Bezier::compute_pose(double t, double &x, double &y) {
     // control points
-    double p0[2] = {0.0, 0.0};
-    double p1[2] = {1.0, 1.0};
-    double p2[2] = {2.0, 1.0};
-    double p3[2] = {3.0, 0.0};
-
-    double x = (1 - t) * ((1 - t) * ((1 - t) * p0[0] + t * p1[0]) +
-                          t * ((1 - t) * p1[0] + t * p2[0])) +
-               t * ((1 - t) * ((1 - t) * p1[0] + t * p2[0]) +
-                    t * ((1 - t) * p2[0] + t * p3[0]));
-    double y = (1 - t) * ((1 - t) * ((1 - t) * p0[1] + t * p1[1]) +
-                          t * ((1 - t) * p1[1] + t * p2[1])) +
-               t * ((1 - t) * ((1 - t) * p1[1] + t * p2[1]) +
-                    t * ((1 - t) * p2[1] + t * p3[1]));
+    x = (1 - t) * ((1 - t) * ((1 - t) * p0[0] + t * p1[0]) +
+                   t * ((1 - t) * p1[0] + t * p2[0])) +
+        t * ((1 - t) * ((1 - t) * p1[0] + t * p2[0]) +
+             t * ((1 - t) * p2[0] + t * p3[0]));
+    y = (1 - t) * ((1 - t) * ((1 - t) * p0[1] + t * p1[1]) +
+                   t * ((1 - t) * p1[1] + t * p2[1])) +
+        t * ((1 - t) * ((1 - t) * p1[1] + t * p2[1]) +
+             t * ((1 - t) * p2[1] + t * p3[1]));
 }
 
 void Bezier::compute_velo(double t) {
+    (void)t;
 }
 
 void Bezier::compute_accl(double t) {
+    (void)t;
 }
 
-void Bezier::create_box_marker(){
+void Bezier::create_marker(const std::string &name, double x, double y, double z) {
+    visualization_msgs::msg::InteractiveMarker int_marker;
+    int_marker.header.frame_id = "base_link";
+    int_marker.name = name;
+    int_marker.description = name;
+    int_marker.pose.position.x = x;
+    int_marker.pose.position.y = y;
+    int_marker.pose.position.z = z;
+
+    visualization_msgs::msg::Marker box;
+    box.type = visualization_msgs::msg::Marker::SPHERE;
+    box.scale.x = 0.2;
+    box.scale.y = 0.2;
+    box.scale.z = 0.2;
+    box.color.a = 1.0;
+    box.color.r = 0.0;
+    box.color.g = 0.5;
+    box.color.b = 0.5;
+
+    visualization_msgs::msg::InteractiveMarkerControl box_control;
+    box_control.always_visible = true;
+    box_control.markers.push_back(box);
+    int_marker.controls.push_back(box_control);
+
+    visualization_msgs::msg::InteractiveMarkerControl move_plane_control;
+    move_plane_control.orientation.w = 1.0;
+    move_plane_control.orientation.x = 0.0;
+    move_plane_control.orientation.y = 1.0;
+    move_plane_control.orientation.z = 0.0;
+    move_plane_control.name = "move_plane";
+    move_plane_control.interaction_mode =
+        visualization_msgs::msg::InteractiveMarkerControl::MOVE_PLANE;
+    int_marker.controls.push_back(move_plane_control);
+
+    server_->insert(
+        int_marker,
+        std::bind(&Bezier::process_feedback, this, std::placeholders::_1));
+}
+
+void Bezier::process_feedback(const IntMarkerFBMsg::ConstSharedPtr &feedback) {
+    if (feedback->marker_name == "start") {
+        p0[0] = feedback->pose.position.x;
+        p0[1] = feedback->pose.position.y;
+    }
+    if (feedback->marker_name == "c1") {
+        p1[0] = feedback->pose.position.x;
+        p1[1] = feedback->pose.position.y;
+    }
+    if (feedback->marker_name == "c2") {
+        p2[0] = feedback->pose.position.x;
+        p2[1] = feedback->pose.position.y;
+    }
+    if (feedback->marker_name == "end") {
+        p3[0] = feedback->pose.position.x;
+        p3[1] = feedback->pose.position.y;
+    }
+    publish_path();
+}
+
+void Bezier::publish_path() {
+    PathMsg path;
+    path.header.stamp = this->get_clock()->now();
+    path.header.frame_id = "base_link";
+    double x;
+    double y;
+    for (size_t i = 0; i < 100; i++) {
+        PoseStampedMsg pose;
+        pose.header = path.header;
+        compute_pose(i / 100.0, x, y);
+        pose.pose.position.x = x;
+        pose.pose.position.y = y;
+        pose.pose.position.z = 0.0;
+        path.poses.push_back(pose);
+    }
+    path_publisher_->publish(path);
+}
+
+int main(int argc, char *argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<Bezier>());
+    rclcpp::shutdown();
+    return 0;
 }
